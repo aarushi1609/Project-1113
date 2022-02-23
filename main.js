f93495736822053591ebe3e38bc7a69df66142d6
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2022-02-23 14:50:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas = createCanvas(640, 480);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 70, 50, 500, 400);
+
+    fill("lightblue");
+    stroke("lightblue");
+    rect(70, 50, 500, 20);
+
+    fill("lightblue");
+    stroke("lightblue");
+    rect(70, 50, 20, 400);
+
+    fill("lightblue");
+    stroke("lightblue");
+    rect(570, 50, 20, 400);
+
+    fill("lightblue");
+    stroke("lightblue");
+    rect(70, 450, 520, 20);
+}
+
